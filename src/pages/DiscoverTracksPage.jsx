c8c9a859dc4e8dd5641597bc6db124effd23b66c
Jake Bowden <jbowden@jahnelgroup.com>
2024-07-08 13:27:46 -0400

    Implemented song bite play and pause button

--- src/pages/DiscoverTracksPage.jsx
+++ src/pages/DiscoverTracksPage.jsx
@@ -1,7 +1,7 @@
 import TrackInput from "../components/TrackInput";
 import { tracks as mockData } from "../utils/mockData.js";
 import TrackCard from "../components/TrackCard.jsx";
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useTrack } from "../context/TrackContext.jsx";
 
 const DiscoverTracks = () => {
@@ -9,8 +9,19 @@ const DiscoverTracks = () => {
   const [tracks, setTracks] = useState(mockData);
   const { addLikedTrack, state } = useTrack();
   const likedTracks = state.likedTracks.map((track) => track.id);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
 
   const currentTrack = tracks[currentTrackIndex];
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.src = currentTrack ? currentTrack.preview_url : "";
+      if (isPlaying) {
+        audioRef.current.play();
+      }
+    }
+  }, [currentTrack]);
 
   const handleLike = () => {
     if (currentTrack && !likedTracks.includes(currentTrack.id)) {
@@ -20,10 +31,23 @@ const DiscoverTracks = () => {
       alert("Track already liked");
     }
     setCurrentTrackIndex((prevIndex) => prevIndex + 1);
+    setIsPlaying(true);
   };
 
   const handlePass = () => {
     setCurrentTrackIndex((prevIndex) => prevIndex + 1);
+    setIsPlaying(true);
+  };
+
+  const togglePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
   };
 
   return (
@@ -33,8 +57,12 @@ const DiscoverTracks = () => {
       {currentTrack ? (
         <>
           <TrackCard track={currentTrack} />
+          <audio ref={audioRef} />
           <button onClick={handleLike}>LIKE</button>
           <button onClick={handlePass}>PASS</button>
+          <button onClick={togglePlayPause}>
+            {isPlaying ? "PAUSE" : "PLAY"}
+          </button>
         </>
       ) : (
         <p>No more tracks!</p>
