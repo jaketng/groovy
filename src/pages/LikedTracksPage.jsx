a75439d68d51ca74b64cb6ae3ae0737b526eff10
Jake Bowden <jbowden@jahnelgroup.com>
2024-07-08 12:50:44 -0400

    Implemented functionality to remove tracks from likedTracks page

--- src/pages/LikedTracksPage.jsx
+++ src/pages/LikedTracksPage.jsx
@@ -2,14 +2,23 @@ import { useTrack } from "../context/TrackContext.jsx";
 import TrackCard from "../components/TrackCard.jsx";
 
 const LikedTracksPage = () => {
-  const { state } = useTrack();
+  const { state, removeLikedTrack } = useTrack();
   const likedTracks = state.likedTracks || [];
+
+  const handleRemove = (trackId) => {
+    removeLikedTrack(trackId);
+  };
 
   return (
     <>
       <h1>Liked Tracks</h1>
       {likedTracks.length > 0 ? (
-        likedTracks.map((track) => <TrackCard key={track.id} track={track} />)
+        likedTracks.map((track) => (
+          <>
+            <TrackCard key={track.id} track={track} />
+            <button onClick={() => handleRemove(track.id)}>REMOVE</button>
+          </>
+        ))
       ) : (
         <p>No liked tracks!</p>
       )}
