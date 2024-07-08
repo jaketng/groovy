832e3c1ea4e98dd790bb32141516e36a177528c2
Jake Bowden <jbowden@jahnelgroup.com>
2024-07-08 09:49:46 -0400

    created input track page and form

---
+++ src/components/TrackInput.jsx
@@ -0,0 +1,12 @@
+const TrackInput = () => {
+  return (
+    <>
+      <form>
+        <input type="text" name="track-input" />
+        <input type="submit" value="Find Songs!" />
+      </form>
+    </>
+  );
+};
+
+export default TrackInput;
--- src/pages/InputTrackPage.jsx
+++ src/pages/InputTrackPage.jsx
@@ -1,5 +1,12 @@
+import TrackInput from "../components/TrackInput.jsx";
+
 const InputTrackPage = () => {
-  return <h1>Input Track Page</h1>;
+  return (
+    <>
+      <h1>Input Track Page</h1>
+      <TrackInput />
+    </>
+  );
 };
 
 export default InputTrackPage;
