2d6688dac4a751ee0130a9151565fce77a73c6ec
Jake Bowden <jbowden@jahnelgroup.com>
2024-08-12 09:54:59 -0400

    Configured proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // This will proxy requests starting with /api to the Spotify API
+      "/api": {
+        target: "https://api.spotify.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
